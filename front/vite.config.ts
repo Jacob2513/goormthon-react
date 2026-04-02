59322e9a3f0bb3404d4de20d44ed362f66e80656
MinyeongKim <156265474+minyeong981@users.noreply.github.com>
2026-04-02 17:41:18 +0900

    chore: pwa 설정

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,9 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "appIcon.png"],
+      manifest: {
+        name: "전허게",
+        short_name: "전허게",
+        lang: "ko-KR",
+        description: "제주도의 숨겨진 이야기를 전허게에서 만나보세요.",
+        start_url: "/onboarding",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "appIcon.png",
+            sizes: "any",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
